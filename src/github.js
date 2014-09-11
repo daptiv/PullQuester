dfd46c6893e8087967e5609f109c6731f51c3f0b
Adam Kalman <akalman@daptiv.com>
2014-09-11 10:25:32 -0700

    add promise wrapper for github api

---
+++ src/github.js
@@ -0,0 +1,55 @@
+var Q = require('q'),
+    GitHubApi = require('github'),
+    github = new GitHubApi({
+        // required
+        version: '3.0.0'
+    });
+
+function authenticate(username, password) {
+    var options = {
+        type: 'basic',
+        username: username,
+        password: password
+    };
+
+    return Q.ninvoke(github, 'authenticate', options);
+}
+
+function getOrganizations() {
+    return Q.ninvoke(github.user, 'getOrgs', {});
+}
+
+function getCollaborators(user, repository) {
+    var options = {
+        user: user,
+        repo: repository,
+        per_page: 100
+    };
+
+    return Q.ninvoke(github.repos, 'getCollaborators', options);
+}
+
+function getMembers(organization) {
+    var options = {
+        org: organization,
+        per_page: 100
+    };
+
+    return Q.ninvoke(github.orgs, 'getMembers', options);
+}
+
+function getUser(username) {
+    var options = {
+        user: username
+    };
+
+    return Q.ninvoke(github.user, 'getFrom', options);
+}
+
+module.exports = {
+    authenticate: authenticate,
+    getOrganizations: getOrganizations,
+    getCollaborators: getCollaborators,
+    getMembers: getMembers,
+    getUser: getUser
+};
