962c0e4bdc6abb0ea5c03592e26753fd440a084a
dizard2001 <darrel.anderson@changepoint.com>
2014-09-12 09:04:01 -0700

    Added test coverage for gitHubWrapper.js

--- test/githubWrapperTests.js
+++ test/githubWrapperTests.js
@@ -10,19 +10,49 @@ describe('githubwrapper.js Tests', function() {
     beforeEach(function() {
         this.sandbox = sinon.sandbox.create();
 
-        this.promptStub = this.sandbox.stub(GitHubApi, 'prompt');
+        //this.gitHubStub = this.sandbox.stub(GitHubApi, 'prompt');
     });
 
     afterEach(function() {
         this.sandbox.restore();
     });
 
-    it('should return a promise', function() {
-        var questions = 'expected questions';
+    it('should return a promise from authenticate', function() {
+        var username = 'TestUser';
+        var password = '154154';
 
-        var result = inquirerWrapper.prompt(questions);
+        var result = gitHubWrapper.authenticate(username, password);
 
         expect(result).to.have.property('then');
+    });
+
+    it('should return a promise from getOrganizations', function() {
+
+      var result = gitHubWrapper.getOrganizations();
+
+      expect(result).to.have.property('then');
+    });
+
+    it('should return a promise from getCollaborators', function() {
+
+      var result = gitHubWrapper.getCollaborators('testUser', 'testRepo');
+
+      expect(result).to.have.property('then');
+    });
+
+    it('should return a promise from getMembers', function() {
+      var organization = 'TestOrg';
+
+      var result = gitHubWrapper.getMembers(organization);
+
+      expect(result).to.have.property('then');
+    });
+
+    it('should returna a promise from getUser', function() {
+      var username = 'testUser'
+      var result = gitHubWrapper.getUser(username);
+
+      expect(result).to.have.property('then');
     });
 
 });
