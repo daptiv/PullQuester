7944325ae51f268637cb1a32710f9f114adbd542
dizard2001 <darrel.anderson@changepoint.com>
2014-09-12 12:11:03 -0700

    Create new version of config

---
+++ src/config.js
@@ -0,0 +1,34 @@
+'use strict';
+var fs = require('fs');
+var configLocation = process.cwd() + '/pullrequest.json';
+
+
+function Configuration(location) {
+    var location = path.resolve(process.cwd(), location);
+
+    this.get = function() {
+        try {
+            return require(location);
+        } catch (error) {
+            return;
+        }
+    };
+
+    this.set = function(config) {
+        try {
+            return fs.writeFileSync(location,  JSON.stringify(config, null, 4));
+        } catch (error) {
+            return;
+        }
+    };
+}
+
+Configuration.default = (function() {
+    return new Configuration(configLocation);
+})();
+
+Configuration.createPathFromId = function(id) {
+    return 'pullrequest.' + id + '.json';
+};
+
+module.exports = Configuration;
