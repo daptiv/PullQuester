4791ef4133317c4d1e78ccb36a82c4ee16b9607a
dizard2001 <darrel.anderson@changepoint.com>
2014-09-12 10:45:50 -0700

    Moved pullrequest.tmpl into src dir and fixed paths referencing this

--- src/template.js
+++ src/template.js
@@ -2,7 +2,7 @@
 var _ = require('lodash');
 var fs = require('fs');
 var templateLocation = process.cwd() + '/pullrequest.tmpl',
-defaultTemplateLocation = __dirname + '/../pullrequest.tmpl';
+defaultTemplateLocation = __dirname + '/pullrequest.tmpl';
 
 function createDefault (){
     var defaultTemplate = fs.readFileSync(defaultTemplateLocation);
