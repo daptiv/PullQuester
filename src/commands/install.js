e305d7903cab77f21c03da47a94eae4f7a38a43d
Adam Kalman <akalman@daptiv.com>
2014-09-12 09:22:53 -0700

    have install.js call to windows install script if on windows.

--- src/commands/install.js
+++ src/commands/install.js
@@ -2,12 +2,21 @@
 
 var exec =  require('child_process').exec;
 var inquirer = require('../inquirerWrapper');
+var os = require('os');
 
 module.exports = function() {
     exec('hub', function(error, stdout, stderr) {
         var matches = stderr.match(/hub: command not found/);
         if (matches && matches.length) {
-            exec('./install-hub.sh', function() { });
+            if (os.platform() === 'win32') {
+                console.log(
+                    'This install script is only supported on windows with ' +
+                    'git bash.  If this fails please try again from git bash.');
+
+                exec('./install-hub.win.sh', function() { });
+            } else {
+                exec('./install-hub.sh', function() { });
+            }
         }
     });
 };
