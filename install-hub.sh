d8cb9df3818bd7a4c83122b15d80dce3614452b0
James Trinklein <james.trinklein@changepoint.com>
2015-02-05 22:50:30 -0800

    update installer scripts to install hub 2.x and Go

--- install-hub.sh
+++ install-hub.sh
@@ -1,10 +1,76 @@
 #!/bin/bash
 
-mkdir .pullquester-temp
-cd .pullquester-temp
-curl -L https://github.com/github/hub/archive/master.zip > master.zip
-tar -xf master.zip
-cd hub-master
-sudo rake install
-cd ../..
-rm -rf .pullquester-temp
+goto_temp_dir() {
+
+    if [ ! -d .pullquester-temp ]; then
+        mkdir .pullquester-temp
+    fi
+    pushd .pullquester-temp
+}
+
+cleanup_temp_dir() {
+    popd
+    rm -r .pullquester-temp
+}
+
+install_go() {
+    local GOVERSION=1.4.1
+    
+    echo Installing Go v$GOVERSION
+    curl -o go.tar.gz "https://storage.googleapis.com/golang/go$GOVERSION.darwin-amd64-osx10.8.tar.gz"
+    tar -C . -xzf go.tar.gz
+    cp ./go/* $GOROOT
+    echo Done!
+}
+
+ensure_GoRoot_dir() {
+    if [ -z "$GOROOT" ]; then
+        export GOROOT=$HOME/go
+    fi
+
+    echo GOROOT=$GOROOT
+
+    if [ ! -d $GOROOT ]; then
+        mkdir -p $GOROOT
+    fi
+}
+
+ensure_GoPath_dir() {
+    if [ -z "$GOPATH" ]; then
+        export GOPATH=$GOROOT/packages
+    fi
+
+    echo GOPATH=$GOPATH
+
+    if [ ! -d $GOPATH ]; then
+        mkdir -p $GOPATH
+    fi
+}
+
+ensure_Go_paths() {
+    ensure_GoRoot_dir
+    ensure_GoPath_dir
+}
+
+install_hub() {
+    echo Installing hub...
+    go get github.com/github/hub
+    go install github.com/github/hub
+    echo Done!
+}
+
+ensure_tmp_dir
+
+# check for go installation
+ensure_Go_paths
+
+if [ -z "$(which go)" ]; then
+    install_go
+fi
+
+#check for package storage location
+ensure_GoPath_dir
+
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+install_hub
--- install-hub.win.sh
+++ install-hub.win.sh
@@ -1,10 +1,84 @@
 #!/bin/bash
 
-mkdir .pullquester-temp
-cd .pullquester-temp
-curl -L https://github.com/github/hub/archive/master.zip > master.zip
-unzip master.zip
-cd hub-master
-rake install
-cd ../..
-rm -rf .pullquester-temp
+bash_path() {
+    echo $1 | sed -e 's#\(.\):#\1#' | sed -e 's#\\#/#g' ;
+}
+
+goto_temp_dir() {
+
+    if [ ! -d .pullquester-temp ]; then
+        mkdir .pullquester-temp
+    fi
+    pushd .pullquester-temp
+}
+
+cleanup_temp_dir() {
+    popd
+    rm -r .pullquester-temp
+}
+
+install_go() {
+    local GOVERSION=1.4.1
+    
+    echo Installing Go v$GOVERSION
+    curl -o go.zip "https://storage.googleapis.com/golang/go$GOVERSION.windows-amd64.zip"
+    unzip go.zip
+    cp ./go/* $GOROOT
+    echo Done!
+}
+
+ensure_GoRoot_dir() {
+    if [ -z "$GOROOT" ]; then
+        export GOROOT=$( bash_path $HOME )/go
+    else
+        export GOROOT=$( bash_path $GOROOT )
+    fi
+
+    echo GOROOT=$GOROOT
+
+    if [ ! -d $GOROOT ]; then
+        mkdir -p $GOROOT
+    fi
+}
+
+ensure_GoPath_dir() {
+    if [ -z "$GOPATH" ]; then
+        export GOPATH=$GOROOT/packages
+    else
+        export GOPATH=$( bash_path $GOPATH )
+    fi
+
+    echo GOPATH=$GOPATH
+
+    if [ ! -d $GOPATH ]; then
+        mkdir -p $GOPATH
+    fi
+}
+
+ensure_Go_paths() {
+    ensure_GoRoot_dir
+    ensure_GoPath_dir
+}
+
+install_hub() {
+    echo Installing hub...
+    go get github.com/github/hub
+    go install github.com/github/hub
+    echo Done!
+}
+
+ensure_tmp_dir
+
+# check for go installation
+ensure_Go_paths
+
+if [ -z "$(which go)" ]; then
+    install_go
+fi
+
+#check for package storage location
+ensure_GoPath_dir
+
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+install_hub
