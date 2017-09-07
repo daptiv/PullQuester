e6271b44c2f70665eb09224d5d56a8b55dc2827b
James Trinklein <james.trinklein@changepoint.com>
2017-09-07 09:59:45 -0700

    feat: add logging to update command

--- src/commands/update.js
+++ src/commands/update.js
@@ -3,11 +3,14 @@
 const fs = require('fs');
 
 function upgrade_from_0_3_0_to_0_4_0() {
-    if (! fs.existsSync('./.pullquester/')) {
-        fs.mkdirSync('.pullquester');
-        fs.renameSync('pullrequest.json', '.pullquester/pullrequest.json');
-        fs.renameSync('pullrequest.tmpl', '.pullquester/pullrequest.tmpl');
+    if (fs.existsSync('./.pullquester/')) {
+      return;
     }
+
+    console.log('applying update from v0.3.0 to v0.4.0');
+    fs.mkdirSync('.pullquester');
+    fs.renameSync('pullrequest.json', '.pullquester/pullrequest.json');
+    fs.renameSync('pullrequest.tmpl', '.pullquester/pullrequest.tmpl');
 }
 
 function upgrade_from_0_7_0_to_0_8_0() {
@@ -15,6 +18,8 @@ function upgrade_from_0_7_0_to_0_8_0() {
     if (config.revision && config.revision >= 2) {
         return;
     }
+
+    console.log('applying update from v0.7.0 to v0.8.0');
     config.revision = 2;
     fs.writeFileSync('./.pullquester/pullrequest.json', JSON.stringify(config, null, 4));
     var template = fs.readFileSync('./.pullquester/pullrequest.tmpl');
@@ -29,6 +34,8 @@ function upgrade_from_0_7_0_to_0_8_0() {
 }
 
 module.exports = function() {
+    console.log('applying updates...');
     upgrade_from_0_3_0_to_0_4_0();
     upgrade_from_0_7_0_to_0_8_0();
+    console.log('finished applying updates.');
 };
