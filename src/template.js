cef60ad2fa1430f6166f3bfae5ac2395a9842832
James Trinklein <james.trinklein@changepoint.com>
2017-09-07 11:30:31 -0700

    chore: update template to use class instead of prototype

--- src/template.js
+++ src/template.js
@@ -1,59 +1,59 @@
 'use strict';
 
-var _ = require('lodash'),
+const _ = require('lodash'),
     fs = require('fs'),
     path = require('path'),
     templateLocation = path.resolve(process.cwd(), '.pullquester', 'pullrequest.tmpl'),
     defaultTemplateLocation = path.resolve(__dirname , '..', 'pullrequest.tmpl');
 
-function Template(location) {
-    location = path.resolve(process.cwd(), '.pullquester', location);
+class Template {
+    constructor(location) {
+        this.location = path.resolve(process.cwd(), '.pullquester', location);
+    }
 
-    this.get = function() {
+    get() {
         try {
             return fs.readFileSync(location);
         } catch (error) {
             return;
         }
-    };
+    }
 
-    this.set = function(template) {
+    set(template) {
         try {
             return fs.writeFileSync(location,  template);
         } catch (error) {
             return;
         }
-    };
-
-    this.compile = function(templateData) {
-        var compiledString = _.template(this.get(), templateData, {
-            variable: 'config'
-        });
-
-        return compiledString;
-    };
-}
-
-Template.createDefaultIfNotExists = function() {
-    try {
-        var template = Template.default;
-        if (!template.get()) {
-            var defaultTemplate = fs.readFileSync(defaultTemplateLocation);
-            template.set(defaultTemplate);
-        }
-    } catch (error) {
-        return false;
     }
 
-    return true;
-};
+    compile(templateData) {
+        const compiledString = _.template(this.get(), templateData, {
+            variable: 'config'
+        });
+        return compiledString;
+    }
 
-Template.default = (function() {
-    return new Template(templateLocation);
-})();
+    static createDefaultIfNotExists() {
+        try {
+            var template = Template.default;
+            if (!template.get()) {
+                var defaultTemplate = fs.readFileSync(defaultTemplateLocation);
+                template.set(defaultTemplate);
+            }
+        } catch (error) {
+            return false;
+        }
+        return true;
+    }
 
-Template.createPathFromId = function(id) {
-    return 'pullrequest.' + id + '.tmpl';
-};
+    static get default() {
+        return new Template(templateLocation);
+    }
+
+    static createPathFromId(id) {
+        return 'pullrequest.' + id + '.tmpl';
+    }
+}
 
 module.exports = Template;
